2cf2e577be7e1d1fb2b854bb966e6606a68a16ca
yashanghan <yashanghan@localhost>
2023-01-17 18:36:04 -0500

    Added course jdbc and commandline runner

---
+++ learn-spring-boot/src/main/java/com/yashedu/springboot/learnspringboot/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,19 @@
+package com.yashedu.springboot.learnspringboot.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+	@Autowired
+	private JdbcTemplate springJdbcTemplate;
+	public static String INSERT_QUERY = """
+			insert into course (id, name, author)
+			values(1,'learn AWS' , ' in28minutes');
+			""";
+
+	public void insert() {
+		springJdbcTemplate.update(INSERT_QUERY);
+	}
+}
